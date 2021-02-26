7be438d08af428529dd8df03803f20c66b473032
diego-kurman <diego.kurman@bandtec.com.br>
2021-02-26 13:08:56 -0300

    Focus-project

---
+++ src/components/Profile.tsx
@@ -0,0 +1,22 @@
+import { useContext } from 'react'
+import { ChallengesContext } from '../contexts/ChallengesContext'
+import styles from '../styles/components/Profile.module.css'
+
+export function Profile() {
+
+  const { level } = useContext(ChallengesContext)
+
+  return (
+    <div className={styles.profileContainer}>
+      <img src="https://github.com/diego-kurman.png" alt="Diego Kurman"/>
+      <div>
+        <strong>Diego Kurman</strong>
+        
+        <p>
+          <img src="icons/level.svg" alt="Level"/>
+          Level {level}
+        </p>
+      </div>
+    </div>
+  )
+}
